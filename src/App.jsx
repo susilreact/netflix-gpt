81ee0a1503dcdabd0cf3b3efc7beb2b95558f501
susilreact <jenasusil@outlook.com>
2025-06-20 11:30:08 +0530

    Vite & Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div className='text-3xl text-green-500'> Hello Susil</div>
+        </>
+    );
+}
+
+export default App;
